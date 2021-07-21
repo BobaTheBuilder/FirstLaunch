5cd5328a3413046e88e4f4dd02b701f3068ec261
BobaTheBob <ivanmorozko97@gmail.com>
2021-07-21 16:17:39 +0300

    Added class GoodWalker

--- source/src/iWalker.kt
+++ source/src/iWalker.kt
@@ -1,3 +1,30 @@
 interface iWalker {
+    val curSpeed: Int
     fun walk()
+    fun stop()
+    fun currentSpeed()
+    fun printAllInfo()
 }
+
+class GoodWalker(): iWalker {
+    override val curSpeed: Int
+        get() = 6
+
+    override fun walk() {
+        println("Good walker is going!")
+    }
+
+    override fun stop() {
+        println("Good walker has stopped going!")
+    }
+
+    override fun currentSpeed() {
+        println("My speed is $curSpeed km/h")
+    }
+
+    override fun printAllInfo() {
+        this.walk()
+        this.currentSpeed()
+    }
+}
+
