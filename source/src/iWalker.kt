c283240552c0f3c720ba9dd49914d248aa9c73d8
BobaTheBob <ivanmorozko97@gmail.com>
2021-07-21 16:22:19 +0300

    Added Zombie Walker

--- source/src/iWalker.kt
+++ source/src/iWalker.kt
@@ -1,3 +1,29 @@
 interface iWalker {
+    val curSpeed: Int
     fun walk()
+    fun stop()
+    fun currentSpeed()
+    fun printAllInfo()
+}
+
+class ZombieWalker(): iWalker {
+    override val curSpeed: Int
+        get() = 1
+
+    override fun walk() {
+        println("BRAINS! BRAINS! BRAINS!")
+    }
+
+    override fun stop() {
+        println("Grrrrr. Ahhhhhh. Uhhhh.")
+    }
+
+    override fun currentSpeed() {
+        println("Zombie speed is $curSpeed km/h")
+    }
+
+    override fun printAllInfo() {
+        this.walk()
+        this.currentSpeed()
+    }
 }
